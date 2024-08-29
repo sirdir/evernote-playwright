03ca65779c4565567cfe01c8f68ef9e7a2c6d957
sirdir <phddir@gmail.com>
2024-08-29 21:20:17 +0200

    temp

--- tests/Evernote.spec.ts
+++ tests/Evernote.spec.ts
@@ -3,9 +3,30 @@ import { test, expect } from '@playwright/test';
 // temp test to do at least smth meaningfull
 test('navigation to login page', async ({ page }) => {
   await page.goto('/');
+  console.log(`|${process.env.TEST_USER}|`);
+  console.log(`|${process.env.TEST_PASSWORD}|`);
 
-  await page.getByRole('button', { name: 'Continue without accepting' }).click();
+  await page.getByRole('button', { name: 'Accept all cookies' }).click();
   await page.getByRole('navigation').getByRole('link', { name: 'Log in' }).click();
 
   await expect(page).toHaveURL('https://accounts.evernote.com/login');
+  await page.locator('#email').pressSequentially(process.env.TEST_USER!);
+  const continueButton = page.getByRole('button', { name: 'Continue', exact: true });
+  await expect(continueButton).toBeEnabled();
+  await continueButton.click();
+
+  await expect(page).toHaveURL('https://accounts.evernote.com/login-with-password');
+
+  await page.getByPlaceholder('Password').pressSequentially(process.env.TEST_PASSWORD!);
+  await page.getByRole('img').nth(3).click();
+  await expect(continueButton).toBeEnabled();
+
+  console.log(`|${await page.locator('#email').inputValue()}|`);
+  console.log(`|${await page.getByPlaceholder('Password').inputValue()}|`);
+
+  await continueButton.click();
+
+  await page.waitForLoadState('domcontentloaded');
+  await page.waitForTimeout(10_000);
+  await expect(page).toHaveURL('https://www.evernote.com/client/web');
 });
