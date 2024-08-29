02e48094b20e0e4de47f2b0d5a1a2674e6681fc1
sirdir <phddir@gmail.com>
2024-08-29 19:27:47 +0200

    add prettier, for better code formatting and readability

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -8,21 +8,21 @@ import { defineConfig, devices } from "@playwright/test";
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   fullyParallel: true,
-  reporter: "html",
+  reporter: 'html',
   use: {
-    baseURL: "https://evernote.com/",
+    baseURL: 'https://evernote.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
   },
 
   /* I'm using only chromium at the  moment, it is overkill to use firefox and etc. for current test assignment */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
 });
--- tests/Evernote.spec.ts
+++ tests/Evernote.spec.ts
@@ -1,16 +1,11 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
 // temp test to do at least smth meaningfull
-test("navigation to login page", async ({ page }) => {
-  await page.goto("/");
+test('navigation to login page', async ({ page }) => {
+  await page.goto('/');
 
-  await page
-    .getByRole("button", { name: "Continue without accepting" })
-    .click();
-  await page
-    .getByRole("navigation")
-    .getByRole("link", { name: "Log in" })
-    .click();
+  await page.getByRole('button', { name: 'Continue without accepting' }).click();
+  await page.getByRole('navigation').getByRole('link', { name: 'Log in' }).click();
 
-  await expect(page).toHaveURL("https://accounts.evernote.com/login");
+  await expect(page).toHaveURL('https://accounts.evernote.com/login');
 });
