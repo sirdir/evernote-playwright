2eb5e02d41d8241c9e4e2f77fba7a1ab5e5953a0
sirdir <phddir@gmail.com>
2024-08-29 19:33:11 +0200

    aadd dotenv libarary to orginize some configuration files wioth env variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
-
+import dotenv from 'dotenv';
+import path from 'node:path';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests',
